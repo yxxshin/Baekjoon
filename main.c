4b04d6fe9b75930ad08b742cce9bfafbbaf570d2
YeonSang <samshin3910@gmail.com>
2020-04-05 19:23:41 +0900

    Baekjoon 11051

--- main.c
+++ main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #define MIN(a,b) ((a)>(b)?(b):(a))
 
-int N, K;
-int main(){
-	scanf("%d %d",&N,&K);
-	K = MIN(K,N-K);
-	
-	int up=1, down=1;	//분자, 분모
-	for(int i=1; i<=K; i++){
-		up *= N--;
-		down *= i;
-	}
-	printf("%d",up/down);
+// nCk 를 arr[n][k] 에 담자
+int arr[1001][1001];
 
+int main(){
+	int N, K;
+	scanf("%d %d",&N,&K);
+	for(int i=1; i<=N; i++){
+		for(int j=0; j<=N; j++){
+			if(j==0 || i==j){
+				arr[i][j]=1;
+				continue;
+			}
+			else arr[i][j] = (arr[i-1][j-1] + arr[i-1][j])%10007; 
+			// nCk + nC(k+1) = (n+1)C(k+1) 임을 이용
+		}
+	}
+
+	printf("%d",arr[N][K]);
 }
