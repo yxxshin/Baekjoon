6630c9413c3071aea1c9d9975082930036d8fead
YeonSang <samshin3910@snu.ac.kr>
2020-05-08 06:22:10 +0900

    Baekjoon 4949

--- main.cpp
+++ main.cpp
@@ -1,41 +1,52 @@
 #include <iostream>
-#include <stack>
 #include <cstring>
+#include <stack>
 
 using namespace std;
 
 int main() {
-	int T, value;
+	char s[105];
+	bool isNo;
 	stack<char> st;
-	char s[50];
 
-	cin >> T;
-	while (T--) {
-		value = 0;
-		cin >> s;
-		for (int i = 0; i < strlen(s); i++)
-			st.push(s[i]);	// save at stack
-
-		while (!st.empty()) {
-			if (st.top() == ')') {
-				value++;
-				st.pop();
-			}
-
-			else if (st.top() == '(') {
-				value--;
-				st.pop();
-			}
-
-			if (value < 0) {
-				break;
-			}
-		}
-		if (st.empty() && value == 0) cout << "YES" << endl;
-		else cout << "NO" << endl;
-
-		while (!st.empty()) {
+	while (1) {
+		isNo = false;
+		while (!st.empty()) {	// empty stack
 			st.pop();
 		}
+
+		for (int i = 0; i < 105; i++)		// empty s
+			s[i] = '\0';
+
+		cin.getline(s, 105);		// input
+
+		if (strcmp(s, ".") == 0) return 0;	// program exit
+		else {
+			for (int i = 0; i < strlen(s); i++) {
+				if (s[i] == '(' || s[i] == '[')
+					st.push(s[i]);	// save open-parenthesis at stack
+
+				// if pair matches, pop the opening one from the stack
+				else if (s[i] == ')') {
+					if (!st.empty() && st.top() == '(')
+						st.pop();
+					else {
+						isNo = true;
+						break;
+					}
+				}
+				else if (s[i] == ']') {
+					if (!st.empty() && st.top() == '[')
+						st.pop();
+					else {
+						isNo = true;
+						break;
+					}
+				}
+			}
+			if (!isNo && st.empty()) cout << "yes" << '\n';
+			else cout << "no" << '\n';
+		}
+
 	}
 }
