a4041e43da5d014177ee366170d94a59bf6bf6a3
YeonSang <samshin3910@snu.ac.kr>
2020-05-08 05:29:30 +0900

    Baekjoon 10773

--- main.cpp
+++ main.cpp
@@ -1,42 +1,22 @@
 #include <iostream>
 #include <stack>
-#include <cstring>
+
 using namespace std;
 
-int main(){
-	stack<int> s;
-	int N, T, temp;
-	cin >> T;
-	while(T--){
-		std::string input;
-		cin >> input;
-		const char *c = input.c_str();
-		if(strcmp(c,"push")==0){
-			cin >> N;
-			s.push(N);
-		}
-		if(strcmp(c,"pop")==0){
-			if(s.empty()) cout << -1 << '\n';
-			else {
-				temp = s.top();
-				cout << temp << '\n';  
-				s.pop();
-			}
-		}
-		if(strcmp(c,"size")==0){
-			temp = s.size();
-			cout << temp << '\n';
-		}
-		if(strcmp(c,"empty")==0){
-			cout << (s.empty() ? 1 : 0 ) << '\n';
-		}
-		if(strcmp(c,"top")==0){
-			if(s.empty()) cout << -1 << '\n';
-			else {
-				temp = s.top();
-				cout << temp << '\n';
-			}
-		}
+int main() {
+	int K, temp, res = 0;
+	stack<int> st;
+	cin >> K;
+	while (K--) {
+		cin >> temp;
+		if (temp != 0)
+			st.push(temp);
+		else if (temp == 0)
+			st.pop();
 	}
-
+	while (!st.empty()) {
+		res += st.top();
+		st.pop();
+	}
+	cout << res << endl;
 }
